bd302db4e443ad21327cc6fb4d8e068ffc88a009
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-02-06 13:45:14 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio4.fecha/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio4.fecha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresa tu fecha de nacimiento (dd-MM-yyyy): ");
+
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime fechaNacimiento))
+            {
+                TimeSpan fechaActual = DateTime.Now - fechaNacimiento;
+                DateTime edadCalcular = DateTime.MinValue + fechaActual;
+
+                int años = edadCalcular.Year - 1;
+                int meses = edadCalcular.Month - 1;
+                int dias = edadCalcular.Day - 1;
+
+                Console.WriteLine("Edad: " + años + " " + "años, " +  meses + " " + "meses, " + dias +" " + "días");
+            }
+        }
+    }
+}
